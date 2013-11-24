0910496bf44e11311136d62e6db9a9cee7625b8b
Wilfredo <Wilfredo@192.168.1.5>
2013-11-23 22:03:15 -0300

    version 1.0

---
+++ src/sistemacontable/TipoCuenta.java
@@ -0,0 +1,60 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistemacontable;
+
+/**
+ *
+ * @author Wilfredo
+ */
+public class TipoCuenta {
+    private int idTipoCuenta;
+    private String nombreCuenta;
+    private String naturaleza;
+
+    /**
+     * @return the idTipoCuenta
+     */
+    public int getIdTipoCuenta() {
+        return idTipoCuenta;
+    }
+
+    /**
+     * @param idTipoCuenta the idTipoCuenta to set
+     */
+    public void setIdTipoCuenta(int idTipoCuenta) {
+        this.idTipoCuenta = idTipoCuenta;
+    }
+
+    /**
+     * @return the nombreCuenta
+     */
+    public String getNombreCuenta() {
+        return nombreCuenta;
+    }
+
+    /**
+     * @param nombreCuenta the nombreCuenta to set
+     */
+    public void setNombreCuenta(String nombreCuenta) {
+        this.nombreCuenta = nombreCuenta;
+    }
+
+    /**
+     * @return the naturaleza
+     */
+    public String getNaturaleza() {
+        return naturaleza;
+    }
+
+    /**
+     * @param naturaleza the naturaleza to set
+     */
+    public void setNaturaleza(String naturaleza) {
+        this.naturaleza = naturaleza;
+    }
+    
+    
+    
+}
